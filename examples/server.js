9f6a85d781198a71a7cb21244f9cfc78ee250a10
eviltik <eviltik@gmail.com>
2019-05-09 04:21:15 +0200

    improve example

--- examples/server.js
+++ examples/server.js
@@ -1,14 +1,54 @@
 const Server = require('../').server;
 const Client = require('../').client;
 
-const client = new Client();
-client.connect();
-client.method('test');
-
-
 const server = new Server();
 server.start();
 
+// mandatory event
 server.on('error', (err) => {
-    console.log(err);
+    throw err;
 });
+
+server.registerMethod(
+    {
+        name:'test2',
+        properties:{
+            'foo':{
+                description:'field example',
+                type:'string'
+            }
+        },
+        required:['foo']
+    },
+    (params, context, callback) => {
+        callback(null, 'you pass '+JSON.stringify(params));
+    }
+);
+
+const client = new Client();
+client.connect();
+
+// no callback, test1 is not a registered method,
+// fail silently (because no callback)
+console.log('calling rpc "test1"');
+client.rpc('test1', { foo:'bar' });
+
+// callback, test2 is a registered method
+console.log('calling rpc "test2"');
+client.rpc('test2', { foo:'bar' }, (err, result) => {
+    if (err) console.log(err);
+    console.log(result);
+});
+
+// async/await, test3 is not a registered method
+(async function test() {
+    try {
+        console.log('calling rpc "test3"');
+        await client.rpcPromise('test3');
+    } catch(e) {
+        console.log(e);
+    }
+
+    // clean exit (not mandatory)
+    client.close(process.exit);
+})();
